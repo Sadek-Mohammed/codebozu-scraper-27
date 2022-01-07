054944d49a70f3b82494a2440ff2fd2a908abd3a
Superhero1015 <codyskinnerhome@gmail.com>
2022-01-07 03:07:09 -0500

    I have added DailyMail functionality

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from utils import parse_wiki_multiple
 from politico import parse_trump
 from bbc import get_bbc_multi
+from dm import get_dm_multi
 
 if __name__ == '__main__':
     # parse_trump("https://www.politico.com/news/magazine/2021/01/18/trump-presidency-administration-biggest-impact-policy-analysis-451479")
-    get_bbc_multi("https://www.bbc.com/news/topics/cp7r8vgl2lgt/donald-trump")
+    #get_bbc_multi("https://www.bbc.com/news/topics/cp7r8vgl2lgt/donald-trump")
+    get_dm_multi("https://www.dailymail.co.uk/news/donald_trump/index.html")
     # links = [
     #     "https://en.wikipedia.org/wiki/Category:21st-century_presidents_of_the_United_States",
     #     "https://en.wikipedia.org/wiki/Category:21st-century_vice_presidents_of_the_United_States",
