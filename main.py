91f72e11ac4dbad3153a42d6a4f174a264707eef
Gabriel Ashkenazi <gabriel.ashkenazi1@gmail.com>
2022-01-09 02:07:48 -0500

    cnn scraping finished + selenium

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from utils import parse_wiki_multiple
 from politico import parse_trump
 from bbc import get_bbc_multi
+from cnn import init
 
 if __name__ == '__main__':
     # parse_trump("https://www.politico.com/news/magazine/2021/01/18/trump-presidency-administration-biggest-impact-policy-analysis-451479")
-    get_bbc_multi("https://www.bbc.com/news/topics/cp7r8vgl2lgt/donald-trump")
+    # get_bbc_multi("https://www.bbc.com/news/topics/cp7r8vgl2lgt/donald-trump")
+
+    politicians_list: list = ['Donald Trump', 'George W. Bush', 'Joe Biden', 'Bill Clinton', 'Barack Obama']
+    init(politicians_list)
+
     # links = [
     #     "https://en.wikipedia.org/wiki/Category:21st-century_presidents_of_the_United_States",
     #     "https://en.wikipedia.org/wiki/Category:21st-century_vice_presidents_of_the_United_States",
