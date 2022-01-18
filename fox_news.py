64354c85ca29b7c78a6927510ea1a8714887e843
tigritik <tigritik@gmail.com>
2022-01-17 20:00:18 -0500

    in depth analysis added for fox news

--- fox_news.py
+++ fox_news.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup as bs4
 from sentiment import analyze_sent
 import time
 import pandas as pd
+import spacy
 
 politicians = [
     "trump",
@@ -14,6 +15,8 @@ politicians = [
 ]
 
 data = {}
+
+in_depth_analysis = False
 
 
 # unnecessary method - scrapes articles from the front page of Fox News
@@ -50,12 +53,13 @@ def get_selenium_source(url, pages):
 
 
 # runs analysis on an article {url} returns (title, text, pos)
-def run_analysis(url):
+def run_analysis(url, politician):
     soup = fetch_link(url)
 
     try:
         title = soup.find("h1", class_="headline").text
         text = soup.find("div", class_="article-body").text
+        text = subject_analysis(text, politician) if in_depth_analysis else text
         pos = analyze_sent(text)['pos'] * 1000
         return (title, text, pos)
     except AttributeError:
@@ -63,6 +67,19 @@ def run_analysis(url):
 
     return None
 
+# returns the sentences that have {politician} as a subject
+def subject_analysis(text, politician):
+    nlp = spacy.load("en_core_web_sm")
+    sentences = text.split(".")
+    l = []
+    for sentence in sentences:
+        subjects = [str(tok).lower() for tok in nlp(sentence) if (tok.dep_ == "nsubj")]
+        if politician in subjects:
+            l.append(sentence)
+    out = ""
+    for s in l:
+        out += (s + ". ")
+    return out
 
 # turns dictionary into a data frame, outputs dataframe as a csv
 def create_csv():
@@ -70,7 +87,10 @@ def create_csv():
 
 
 # main execution method to scrape info about politicians and Fox News
-def fox_news():
+def fox_news(ida):
+    global in_depth_analysis
+    in_depth_analysis = ida
+    
     for politician in politicians:
         data[politician] = {
             "title": [],
@@ -79,7 +99,7 @@ def fox_news():
         }
         articles = find_by_search(politician)
         for article in articles:
-            result = run_analysis(article)
+            result = run_analysis(article, politician)
             if result is None:
                 continue
             data[politician]["title"].append(result[0])
@@ -87,3 +107,15 @@ def fox_news():
             data[politician]["pos"].append(result[2])
 
     create_csv()
+
+#Interesting test case code below:
+#Notice how the positivity value changes for Donald Trump
+
+#Runs normal analysis
+#print("Normal test:", run_analysis("https://www.foxnews.com/politics/trump-arizona-biden-destructive-florence-rally", "trump")[2])
+
+#Runs in depth analysis
+#in_depth_analysis = True
+#print("In Depth Test:", run_analysis("https://www.foxnews.com/politics/trump-arizona-biden-destructive-florence-rally", "trump")[2])
+
+#Results are printed
